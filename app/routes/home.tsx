4beb06ea3908fcc21fdb00a739189ce6e1087d0d
Bartosz Trusiński <bartosztrusinski@gmail.com>
2026-03-25 15:19:42 +0100

    cleanup; config biome

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,9 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
-
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
-}
-
 export default function Home() {
-  return <Welcome />;
+	return (
+		<>
+			<title>Googol</title>
+			<meta name="description" content="Googol frontend for searching the web" />
+			<h1 className="text-6xl mt-4 font-bold text-center">Googol</h1>
+		</>
+	);
 }

5631e714ce49c7ee585fe4ade4cfae8083742e40
Bartosz Trusiński <bartosztrusinski@gmail.com>
2026-04-07 02:53:45 +0200

    refactor related searches rendering

--- app/components/search-results.tsx
+++ app/components/search-results.tsx
@@ -1,5 +1,5 @@
 import { ArrowDown } from 'lucide-react';
-import { Fragment, useState, useTransition } from 'react';
+import { useState, useTransition } from 'react';
 import { Link } from 'react-router';
 import { RelatedSearches } from '~/components/related-searches';
 import { Button } from '~/components/ui/button';
@@ -32,34 +32,32 @@ export function SearchResults({ results, relatedSearches, page, query }: Props) 
 	return (
 		<>
 			<ul className="space-y-4">
-				{searchResults.map((result, index) => (
-					<Fragment key={result.link}>
-						<li>
-							<Card className="shadow gap-3">
-								<Link to={result.link} target="_blank" rel="noopener noreferrer" className="group">
-									<CardHeader className="gap-1.5">
-										<CardTitle>
-											<h2 className="text-xl text-blue-600 group-visited:text-purple-700 dark:text-blue-300 dark:group-visited:text-purple-400 font-normal leading-snug">
-												{result.title}
-											</h2>
-										</CardTitle>
-										<CardDescription className="text-xs row-start-1">
-											{new URL(result.link).origin}
-										</CardDescription>
-									</CardHeader>
-								</Link>
-								<CardContent>
-									<p className="text-pretty text-muted-foreground">{result.snippet}</p>
-								</CardContent>
-							</Card>
-						</li>
-						{relatedSearches && index === results.length - 1 && (
-							<li>
-								<RelatedSearches searches={relatedSearches.map(({ query }) => query)} />
-							</li>
-						)}
-					</Fragment>
+				{searchResults.map((result) => (
+					<li key={result.link}>
+						<Card className="shadow gap-3">
+							<Link to={result.link} target="_blank" rel="noopener noreferrer" className="group">
+								<CardHeader className="gap-1.5">
+									<CardTitle>
+										<h2 className="text-xl text-blue-600 group-visited:text-purple-700 dark:text-blue-300 dark:group-visited:text-purple-400 font-normal leading-snug">
+											{result.title}
+										</h2>
+									</CardTitle>
+									<CardDescription className="text-xs row-start-1">
+										{new URL(result.link).origin}
+									</CardDescription>
+								</CardHeader>
+							</Link>
+							<CardContent>
+								<p className="text-pretty text-muted-foreground">{result.snippet}</p>
+							</CardContent>
+						</Card>
+					</li>
 				))}
+				{relatedSearches && (
+					<li>
+						<RelatedSearches searches={relatedSearches.map(({ query }) => query)} />
+					</li>
+				)}
 			</ul>
 			<div className="h-10">
 				{hasMoreResults ? (
